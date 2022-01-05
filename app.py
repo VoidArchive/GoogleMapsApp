d993b9b9078660f3318420308534e0432f3751cb
voidarchive <voidarchived@gmail.com>
2022-01-05 15:18:44 +0545

    api request

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import random
 import time
 
-from werkzeug.wrappers import response
 import api
 import googlemaps
 from flask import Flask, render_template, redirect, url_for, jsonify, request
@@ -13,16 +13,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///search_list.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Create database 
-class Search(db.Model):
+# Create database
+
+
+class SearchList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     address = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 # db.create_all()
 
 # Using google api to search
+
+
 def search_google(search_string, radius):
     # Api key import from api.py file for github to not show security warning
     api_key = api.API_KEY
@@ -60,12 +67,34 @@ def search_google(search_string, radius):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
+
+        # need to delete data in database before calling google api 
+        db.session.query(SearchList).delete()
         keyword = request.form['search']
         distance = request.form['distance']
-        response_list = search_google(keyword,distance)
+        response_list = search_google(keyword, distance)
+        for item in response_list:
+            new_data = SearchList(
+                name=item['name'],
+                rating=item['rating'],
+                address=item['vicinity']
+            )
+            db.session.add(new_data)
+        db.session.commit()
 
+        data_list = db.session.query(SearchList).all()
+
+        return render_template('index.html', data_list=data_list)
 
     return(render_template('index.html'))
+
+@app.route('/api_request')
+def api_request():
+    places = db.session.query(SearchList).first()
+    if places:
+        return jsonify(places.to_dict())
+    else:
+        return jsonify({'fail':'fail'})
 
 
 if __name__ == "__main__":
